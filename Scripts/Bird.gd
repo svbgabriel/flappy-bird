a505752b43669eb187bc56978fc1545b2d6db802
Gabriel Batista <svbgabriel@gmail.com>
2020-04-10 16:00:22 -0300

    Implementa a colisão com o Pipe

--- Scripts/Bird.gd
+++ Scripts/Bird.gd
@@ -4,3 +4,7 @@ func _input(event):
 	if event is InputEventScreenTouch:
 		if event.pressed:
 			linear_velocity = Vector2(0, -1000)
+
+func kill():
+	set_process_input(false)
+	apply_impulse(Vector2(0, 0), Vector2(-1000, 0))
